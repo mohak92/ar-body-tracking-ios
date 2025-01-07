328ded50b063b53cce33987647255a46ccea288f
mohak92 <tamhane.mohak@gmail.com>
2025-01-06 21:50:44 -0500

    Initial Commit

---
+++ ar-body-tracking-ios/ar_body_tracking_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ar_body_tracking_iosApp.swift
+//  ar-body-tracking-ios
+//
+//  Created by Mohak Tamhane on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ar_body_tracking_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
